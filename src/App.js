bd554c357fdb8562e7d6f7efb816578075c952d0
felipecardosodeoliveira <felipecardosodeoliveira02@gmail.com>
2020-04-21 19:20:13 -0300

    inicializando projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+
+export default function App() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+}
+
+
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './menu-item.component.styles.scss';
+
+export default function MenuItem({ title, subtitle, imageUrl, size }) {
+    return (
+        <div className={`${size} menu-item`}> 
+            <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}></div>
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+    )
+}
