55b0afd6560a1f1cc2c2ee3dc8dcbb189dcac99d
felipecardosodeoliveira <felipecardosodeoliveira02@gmail.com>
2020-04-21 20:30:50 -0300

    melhorando menu-item e inserindo react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+function HatsPage (props) {
+  console.log(props);
+  
+  return (
+    <div>
+      HATS PAGE
+    </div>
+  )
+}
+
 export default function App() {
     return (
       <div>
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop/hats" component={HatsPage} />
+        </Switch>
       </div>
     );
 }
