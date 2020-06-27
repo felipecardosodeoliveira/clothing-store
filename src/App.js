5d27a464af146c3b8db3f9640ac50a3123b52f3d
felipecardosodeoliveira <felipecardosodeoliveira02@gmail.com>
2020-06-27 15:07:57 -0300

    alterando projeto, criando área com todos os produtos

--- src/App.js
+++ src/App.js
@@ -4,67 +4,14 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-function HomePage1(props) {
-  return (
-    <div>
-      <h1>HOMEPAGE ONE</h1>
-      <button onClick={() => props.history.push('/shop/hats')}>HatsPage</button>
-    </div>
-  )
-}
-
-function HatsPage (props) {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-      <button onClick={() => props.history.push('/')}>GO BACK</button>
-    </div>
-  )
-}
-
-function JacketsPage(props) {
-  return (
-    <div>
-      Jackets Page
-    </div>
-  )
-}
-
-function SneakersPage(props) {
-  return (
-    <div>
-      Sneakers Page
-    </div>
-  )
-}
-
-function WomansPage(props) {
-  return (
-    <div>
-      Womans Page
-    </div>
-  )
-}
-
-function MensPage(props) {
-  return (
-    <div>
-      Mens Page
-    </div>
-  )
-}
+import ShopPage from './pages/shop/shop.component';
 
 export default function App() {
     return (
       <div>
         <Switch>
-          <Route exact path="/" component={HomePage1} />
-          <Route path="/shop/hats" component={HatsPage} />
-          <Route path="/shop/jackets" component={JacketsPage} />
-          <Route path="/shop/sneakers" component={SneakersPage} />          
-          <Route path="/shop/womens" component={WomansPage} />  
-          <Route path="/shop/mens" component={MensPage} />  
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
         </Switch>
       </div>
     );
